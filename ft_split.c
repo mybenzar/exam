85af2ccce1e4182967b0774bb170a4793e91eacf
Myma Bz <mymabz@Mymas-Mac-mini.local>
2019-03-05 21:57:50 +0100

    ajout de ft_split

---
+++ ft_split.c
@@ -0,0 +1,41 @@
+
+/* malloc de 256 pour tab 
+** boucle while pour que, tant qu'on trouve un espace, on skip
+** boucle while pour parcourir str
+** malloc de 4096 pour tab[k]
+** boucle while pour que, tant que ce n'est pas un espace, on donne a tab[k][j] la valeur str[i]
+** boucle while pour que, tant qu'on trouve un espace, on incremente i
+** fermer la ligne
+** fermer le tableau
+*/
+
+#include <stdlib.h>
+
+char		**ft_split(char *str)
+{
+		int		i;
+		int		j;
+		int		k;
+		char	**tab;
+
+		i = 0;
+		k = 0;
+		if (!(split = (char **)malloc(sizeof(char *) * 256)))
+				return (NULL);
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+				i += 1;
+		while (str[i])
+		{
+				j = 0;
+				if (!(split[k] = (char *)malloc(sizeof(char) * 4096)))
+						return (NULL);
+				while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i])
+						split[k][j++] = str[i++];
+				while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+						i += 1;
+				split[k][j] = '\0';
+				k += 1;
+		}
+		split[k] = NULL;
+		return (split);
+}
